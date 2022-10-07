417b7bf18cec8086aec17686ed6451784d379b81
Anil Kumar M C <anirai.kumar@gmail.com>
2022-10-07 10:53:09 +0530

    added reload button

--- src/App.js
+++ src/App.js
@@ -6,36 +6,90 @@ import moment from "moment";
 
 function App() {
   const [commits, setCommits] = useState([]);
-
-  const octokit = new Octokit({
-    auth: "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",
-  });
+  const [isTokenFound, setIsTokenFound] = useState(null);
+  // const [accessToken, setAccessToken] = useState(null);
+  const [octokitConfig, setOctokitConfig] = useState(null);
+  const [isFetching, setIsfetching] = useState(false);
 
   useEffect(() => {
-    fetchCommits();
+    let isFound = localStorage.getItem("access_token");
+    if (isFound) {
+      setIsTokenFound(true);
+      const octokit = new Octokit({
+        auth: isFound,
+      });
+      // setAccessToken(isFound);
+      setOctokitConfig(octokit);
+    } else {
+      setIsTokenFound(false);
+    }
   }, []);
 
+  useEffect(() => {
+    if (isTokenFound) fetchCommits();
+  }, [isTokenFound]);
+
   const fetchCommits = async () => {
-    const { data } = await octokit.request("/repos/anilasdev/gcommits/commits");
+    const { data } = await octokitConfig.request(
+      "/repos/anilasdev/gcommits/commits"
+    );
     console.log(data);
     setCommits(data);
+    setTimeout(() => setIsfetching(false), 1000);
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    let body = {};
+    formData.forEach((value, key) => {
+      body[key] = value;
+    });
+    console.log(body);
+    localStorage.setItem("access_token", body.access_token);
+    window.location.reload();
+  };
+  const onRefresh = () => {
+    setIsfetching(true);
+    fetchCommits();
+  };
+  console.log(isTokenFound, "isTokenFound");
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div className="commits">
-        {commits.map((commit) => (
-          <div className="commit">
-            <p>{commit.commit.message}</p>
-            <span className="">
-              {moment(commit.commit.committer.date).format("DD/MM/YYYY")} by{" "}
-              {""}
-              {commit.commit.committer.name}
-            </span>
+      {/* <header className="App-header"></header> */}
+      {!isTokenFound && (
+        <form onSubmit={onSubmit}>
+          <input
+            placeholder="Please provide your personal access token"
+            required
+            name="access_token"
+          ></input>
+          <button className="" type="submit">
+            Submit
+          </button>
+        </form>
+      )}
+      {isTokenFound && (
+        <>
+          <div
+            class={`reloadSingle ${isFetching ? "spin" : ""}`}
+            onClick={onRefresh}
+            style={{ transform: [{ rotate: "360deg" }] }}
+          ></div>
+          <div className="commits">
+            {commits.map((commit) => (
+              <div className="commit">
+                <p>{commit.commit.message}</p>
+                <span className="">
+                  {moment(commit.commit.committer.date).format("DD/MM/YYYY")} by{" "}
+                  {""}
+                  {commit.commit.committer.name}
+                </span>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>
   );
 }
