cfe784ff045282eceafb1092f0874f2ae2eb7db8
Anil Kumar M C <anirai.kumar@gmail.com>
2022-10-07 10:02:46 +0530

    list commits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import { Octokit } from "@octokit/core";
+import moment from "moment";
 
 function App() {
+  const [commits, setCommits] = useState([]);
+
   const octokit = new Octokit({
-    auth: "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",
+    auth: "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",
   });
 
   useEffect(() => {
@@ -14,12 +18,24 @@ function App() {
   const fetchCommits = async () => {
     const { data } = await octokit.request("/repos/anilasdev/gcommits/commits");
     console.log(data);
+    setCommits(data);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
+      <div className="commits">
+        {commits.map((commit) => (
+          <div className="commit">
+            <p>{commit.commit.message}</p>
+            <span className="">
+              {moment(commit.commit.committer.date).format("DD/MM/YYYY")} by{" "}
+              {""}
+              {commit.commit.committer.name}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
